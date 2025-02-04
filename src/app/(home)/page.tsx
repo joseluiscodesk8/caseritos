6da763b34afcf970fcd56a297c334468ce61ff56
jose luis gomez angarita <jlgomezangarita@gmail.com>
2025-02-03 21:23:58 -0500

    caseritos4

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,35 +2,11 @@
 
 import { NextPage } from "next";
 import Link from "next/link";
-import { motion } from "framer-motion";
-import Image from "next/image";
 import Logo from "../components/Logo";
 
 const Home: NextPage = () => {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-      {/* Imagen de fondo animada */}
-      <motion.div
-        className="absolute inset-0 w-full h-full"
-        initial={{ x: "-10%" }}
-        animate={{ x: "10%" }}
-        transition={{
-          duration: 10,
-          repeat: Infinity,
-          repeatType: "mirror",
-          ease: "linear",
-        }}
-      >
-        <Image
-          src="/tres.jpeg" // Cambia la ruta por la de tu imagen
-          alt="Fondo animado"
-          layout="fill"
-          objectFit="cover"
-          quality={100}
-        />
-      </motion.div>
-
-      {/* Contenido */}
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-white">
         <Logo />
         <section className="flex gap-4 mt-6">
